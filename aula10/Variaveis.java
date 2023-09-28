e1f05a9ce7d2eaff120e1381d98ec465b3144a28
Livia Braz <revoltz.282@gmail.com>
2023-09-28 09:55:37 -0300

    +aula 10

---
+++ aula10/Variaveis.java
@@ -0,0 +1,21 @@
+package aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		//convenção java
+		int idade = 21;
+		String nome = "Gloria";
+		String nomeDoMeuGato = "Toddy";
+		int ano2022;
+		
+		idade = 18;
+		
+		System.out.println("Idade = " + idade);
+		System.out.println("Nome = " + nome);
+		System.out.println("Nome do meu gato = " + nomeDoMeuGato);
+		
+	}
+
+}
